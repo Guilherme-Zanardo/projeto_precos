b9d885fa18e7cc3b5c8a57440b0cfc3c69771d48
Guilherne-Zanardo <guilherme.zanardo@outlook.com>
2026-08-13 11:42:58 -0300

    produtos

--- func_list_prod.py
+++ func_list_prod.py
@@ -1,17 +1,18 @@
 # Função de tabela central de correspondência de produtos entre IBGE e SEAB
 
 PRODUTOS = {
-    "Ovo de galinha": {"ibge": 7355, "seab": 1791},
-    "Contrafilé": {"ibge": 7291, "seab": 1645},
     "Acém": {"ibge": 7300, "seab": 1665},
-    "Costela": {"ibge": 7302, "seab": 1673},
     "Carne de porco": {"ibge": 7287, "seab": 1635},
-    "Linguiça": {"ibge": 7339, "seab": 1771},
-    "Frango inteiro": {"ibge": 107617, "seab": 1789},
-    "Sardinha": {"ibge": 7310, "seab": 1691},
-    "Milho (em grão)": {"ibge": 47618, "seab": None},
-    "Banha de porco": {"ibge": 101499, "seab": None},
+    "Contrafilé": {"ibge": 7291, "seab": 1645},
+    "Costela": {"ibge": 7302, "seab": 1673},
     "Filé-mignon": {"ibge": 7292, "seab": None},
+    "Frango inteiro": {"ibge": 107617, "seab": 1789},
+    "Linguiça": {"ibge": 7339, "seab": 1771},
+    "Milho (em grão)": {"ibge": 47618, "seab": None},
+    "Ovo de galinha": {"ibge": 7355, "seab": 1791},
+    "Picanha": {"ibge": 47621, "seab": 1691},
+    "Peixe - sardinha": {"ibge": 7310, "seab": None},
+    # "Banha de porco": {"ibge": 101499, "seab": None}, # Não há registro de variação recente
 }
 
 def obter_codigos(origem):
